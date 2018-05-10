5432cefeb0f93038670b469f5217961b4f59ca41
David Krebs <davkrebs@gmail.com>
2018-05-10 22:06:42 +0200

    update submodules and init script

--- init.sh
+++ init.sh
@@ -1,45 +1,68 @@
 #!/bin/bash
 
 error=0
+inst=0
+if [[ $1 == -install ]]; then
+  inst=1
+fi
+
 echoerr() { echo "$@" 1>&2; }
+apt() { 
+  if [[ $inst -eq "1" ]]; then
+    apt install "$@"
+  else
+    echoerr "Please install $@"
+  fi
+}
+
+apt() { 
+  if [[ $inst -eq "1" ]]; then
+    pip install "$@"
+  else
+    echoerr "Please do pip install $@"
+  fi
+}
+
+
+apt=1
 
 if ! [ -x "$(command -v javac)" ]; then
   echoerr "Please install a JDK"
-  echo "apt install openjdk-8"
+  apt "openjdk-8"
   error=1
 fi
 
 if ! [ -x "$(command -v mvn)" ]; then
   echoerr "Please install maven"
-  echo "apt install maven"
+  apt "maven"
   error=1
 fi
 
 if ! [ -x "$(command -v dot)" ]; then
   echoerr "Please install graphviz"
-  echo "apt install graphviz"
+  apt "graphviz"
   error=1
 fi
 
 if ! $(perl -e 'use DBI;'); then
   echoerr "Please install libdbi-perl"
-  echo "apt install libdbi-perl"
+  apt "libdbi-perl"
   error=1
 fi
 
 if ! $(python -c 'import causality; import sklearn') ; then
   echoerr "Please install python, pip, gfortran, python-dev, liblapack-dev"
   echoerr "Also install the python modules numpy, causality, sklearn"
-  echo "apt install python-pip gfortran python-dev liblapack-dev"
-  echo "pip install numpy"
-  echo "pip install causality"
-  echo "pip install sklearn"
+  apt "python-pip gfortran python-dev liblapack-dev"
+  pip "numpy"
+  pip "causality"
+  pip "sklearn"
   error=1
 fi
 
 
-if [ "$error" -eq "1" ]; then
-   echoerr "Please install all dependencies and run again"
+if [ "$error" -eq "1" && "$inst" -eq "0" ]; then
+   echoerr "Please install all dependencies and run again. Calling this script with -install flag will install everything on systems with apt-get."
    exit 1;
 fi
 
